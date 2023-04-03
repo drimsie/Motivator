67e833802564231b72ca124a93b1b95b6c6957c6
mrcreker0 <107767191+mrcreker0@users.noreply.github.com>
2023-04-03 17:58:07 +0400

    21

---
+++ My project (1)/Assets/c# scripts/buttonControl.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonControl : MonoBehaviour
+{
+    [SerializeReference] private object _register;   
+}
